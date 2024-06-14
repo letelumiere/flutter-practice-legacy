b394054f11f43b0b60cfe99b955cae87b9dc5d37
letelumiere <letelumiere@gmail.com>
2024-06-14 17:24:13 +0900

    lecture 29 - webview(mised)

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,102 @@
-import 'package:flutter/material.dart';
-import 'package:project/notification.dart';
-  
+import "package:flutter/material.dart";
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  FlutterLocalNotification.init();
   runApp(const MyApp());
 }
-class MyApp  extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context){
-    return const MaterialApp(title: "Flutter App", home: HomePage());
-  }  
-}
 
-class HomePage extends StatefulWidget{
-  const HomePage({Key?key}) : super(key:key);
-  
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-
-class _HomePageState extends State<HomePage>{
-  @override
-  void initState() {
-    FlutterLocalNotification.init();
-    Future.delayed(
-        const Duration(seconds: 3), 
-        FlutterLocalNotification.requestNotificationPermisson()
-      );
-    super.initState();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("flutter App"),
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: WebViewPage(),
+      );
+  }
+}
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+  
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+  
+  @override
+  void initState() {
+    super.initState();
+
+    late final PlatformWebViewControllerCreationParams params;
+    
+    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
+      params = WebKitWebViewControllerCreationParams(
+        allowsInlineMediaPlayback: true,
+        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
+      );        
+    } else {
+      params = const PlatformWebViewControllerCreationParams();
+    }
+    
+    final WebViewController controller =
+      WebViewController.fromPlatformCreationParams(params);
+        
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          debugPrint('WebView is loading (progress : $progress%)');
+        },
+        onPageStarted: (String url) {
+          debugPrint('Page started loading: $url');
+        },
+        onPageFinished: (String url) {
+          debugPrint('Page finished loading: $url');
+        },
+        onWebResourceError: (WebResourceError error) {
+          debugPrint('''Page resource error:
+            code: ${error.errorCode}
+            description: ${error.description}
+            errorType: ${error.errorType}
+            isForMainFrame: ${error.isForMainFrame}
+          ''');
+        },
+        onNavigationRequest: (NavigationRequest request) {
+          debugPrint('allowing navigation to ${request.url}');
+          return NavigationDecision.navigate;
+        },
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: () async{
-            try{
-              await FlutterLocalNotification.showNotification();
-            }catch(e){
-              print(e);
-            }
-          },
-          child: const Text("알림 보내기"),
-        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+            ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev/'));
+      if (controller.platform is AndroidWebViewController) {
+        AndroidWebViewController.enableDebugging(true);
+        (controller.platform as AndroidWebViewController)
+          .setMediaPlaybackRequiresUserGesture(false);
+      }
+      _controller = controller;
+    }
+    
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: WebViewWidget(controller: _controller),
       ),
     );
-  }  
+  }
 }
