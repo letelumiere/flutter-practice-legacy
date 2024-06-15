cc003bad529e5f318715bd74308abd319100e2a2
letelumiere <letelumiere@gmail.com>
2024-06-15 13:09:36 +0900

    lecture 30 - form submit

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,122 @@
 import "package:flutter/material.dart";
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import "package:project/successPage.dart";
+
+import "package:project/user.dart";
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter App',
-      home: WebViewPage(),
-      );
-  }
-}
-class WebViewPage extends StatefulWidget {
-  const WebViewPage({super.key});
-  
-  @override
-  State<WebViewPage> createState() => _WebViewPageState();
-}
-class _WebViewPageState extends State<WebViewPage> {
-  late final WebViewController _controller;
-  
-  @override
-  void initState() {
-    super.initState();
-
-    late final PlatformWebViewControllerCreationParams params;
-    
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );        
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-    
-    final WebViewController controller =
-      WebViewController.fromPlatformCreationParams(params);
-        
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          debugPrint('WebView is loading (progress : $progress%)');
-        },
-        onPageStarted: (String url) {
-          debugPrint('Page started loading: $url');
-        },
-        onPageFinished: (String url) {
-          debugPrint('Page finished loading: $url');
-        },
-        onWebResourceError: (WebResourceError error) {
-          debugPrint('''Page resource error:
-            code: ${error.errorCode}
-            description: ${error.description}
-            errorType: ${error.errorType}
-            isForMainFrame: ${error.isForMainFrame}
-          ''');
-        },
-        onNavigationRequest: (NavigationRequest request) {
-          debugPrint('allowing navigation to ${request.url}');
-          return NavigationDecision.navigate;
-        },
-      ),
-      )
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-            ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev/'));
-      if (controller.platform is AndroidWebViewController) {
-        AndroidWebViewController.enableDebugging(true);
-        (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-      }
-      _controller = controller;
-    }
-    
-    @override
-    Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: WebViewWidget(controller: _controller),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/success': (context) => const SuccessPage(),
+      },
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _key = GlobalKey<FormState>();
+  late String _username, _email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test App"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              usernameInput(),
+              const SizedBox(height: 15),
+              emailInput(),
+              const SizedBox(height: 15),
+              submitButton(),
+            ],
+          )  
+        ),
+      ),
+    );
+  }
+
+  Widget usernameInput(){
+    return TextFormField(
+      autofocus: true,
+      validator: (val){
+        if(val!.isEmpty){
+          return 'The input is empty';
+        }else{
+          return null;
+        }
+      },
+      onSaved: (username) => _username = username as String,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Input your username,',
+        labelText: 'Username',
+        labelStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      )
+    );
+  }
+
+  Widget emailInput(){
+    return TextFormField(
+      autofocus: true,
+      validator: (val){
+        if(val!.isEmpty){
+          return 'The input is empty';
+        }else{
+          return null;
+        }
+      },
+      onSaved: (email) => _email = email as String,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Input your email adress,',
+        labelText: 'Email Adress',
+        labelStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      )
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: (){
+        if(_key.currentState!.validate()){
+          _key.currentState!.save();
+          Navigator.pushNamed(context, '/success', arguments: User(_username, _email));
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: const Text("제출", style: TextStyle(fontSize: 10)),
+      )
+    );
+  }
+    
+} 
