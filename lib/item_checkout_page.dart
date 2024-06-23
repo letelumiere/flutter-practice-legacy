8d796fcea3b5d9926fcbe3df7e2b33d697109d39
letelumiere <letelumiere@gmail.com>
2024-06-23 19:30:35 +0900

    commits.

--- lib/item_checkout_page.dart
+++ lib/item_checkout_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:example_project/constants.dart';
 import 'package:example_project/model/product.dart';
+import 'package:kpostal/kpostal.dart';
 
 class ItemCheckoutPage extends StatefulWidget {
   const ItemCheckoutPage({super.key});
@@ -230,15 +231,46 @@ class _ItemCheckoutPageState extends State<ItemCheckoutPage> {
   Widget receiverZipTextField() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: TextFormField(
-        controller: receiverZipController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          hintText: "우편번호",
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              readOnly: true,
+              controller: receiverZipController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "우편번호",
+              ),
+            ),
+          ),
+          const SizedBox(width: 15),
+          FilledButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
+                return KpostalView(
+                  callback: (Kpostal result){
+                    receiverZipController.text = result.postCode;
+                    receiverAddress1Controller.text = result.address;
+                  },
+                );
+              },
+              ));
+            },
+          style: FilledButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 22.0),
+            child: Text("우편 번호 찾기"),
+          ),
         ),
-      ),
-    );
-  }
+      ],
+    ),
+  );
+}
 
   Widget receiverAddress1TextField() {
     return Padding(
@@ -275,6 +307,7 @@ class _ItemCheckoutPageState extends State<ItemCheckoutPage> {
           border: OutlineInputBorder(),
           hintText: "비회원 주문조회 비밀번호",
         ),
+        obscureText: true,
       ),
     );
   }
@@ -288,6 +321,7 @@ class _ItemCheckoutPageState extends State<ItemCheckoutPage> {
           border: OutlineInputBorder(),
           hintText: "비회원 주문조회 비밀번호 확인",
         ),
+        obscureText: true,
       ),
     );
   }
@@ -323,10 +357,12 @@ class _ItemCheckoutPageState extends State<ItemCheckoutPage> {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: cardExpiredDateController,
+        maxLength: 6,
         decoration: const InputDecoration(
           border: OutlineInputBorder(),
-          hintText: "카드 만료일",
+          hintText: "카드 만료일(YYYYMM)",
         ),
+        obscureText: true,
       ),
     );
   }
@@ -336,10 +372,12 @@ class _ItemCheckoutPageState extends State<ItemCheckoutPage> {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: cardPwdTwoDigitsController,
+        maxLength: 2,
         decoration: const InputDecoration(
           border: OutlineInputBorder(),
-          hintText: "카드 비밀번호 앞2자리",
+          hintText: "카드 만료일(YYYYMM)",
         ),
+        obscureText: true,
       ),
     );
   }
