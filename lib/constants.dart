0e50ff652df290766e1c1dabeee405849183069c
letelumiere <letelumiere@gmail.com>
2024-06-23 18:58:09 +0900

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:intl/intl.dart';
+import 'package:example_project/model/product.dart';
+
+final NumberFormat numberFormat = NumberFormat('###,###,###,###');
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:example_project/item_list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      home: const ItemListPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+    );
+  }
+}
