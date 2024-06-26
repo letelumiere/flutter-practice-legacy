77951f93b016838552df684d010dd134416751f7
letelumiere <letelumiere@gmail.com>
2024-06-26 20:20:14 +0900

    commits. and wtf?

---
+++ lib/my_order_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyOrderListPage extends StatefulWidget {
+  const MyOrderListPage({super.key});
+
+  @override
+  State<MyOrderListPage> createState() => _MyOrderListPageState();
+}
+
+class _MyOrderListPageState extends State<MyOrderListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
