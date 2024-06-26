4035ae565a05c0a905939b86423f207da44d3cfb
letelumiere <letelumiere@gmail.com>
2024-06-26 17:09:30 +0900

    commits.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+
+import 'package:example_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:example_project/constants.dart';
 import 'package:example_project/item_list_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
 
